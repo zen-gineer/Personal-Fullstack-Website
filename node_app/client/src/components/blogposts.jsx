e4638defe596bfce374e3be040d2ba67827e68b7
zen-gineer <jshtaway@gmail.com>
2019-04-16 16:56:37 -0700

    updated

--- node_app/client/src/components/blogposts.jsx
+++ node_app/client/src/components/blogposts.jsx
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import Request from 'superagent';
+// import Request from 'superagent';
 
 class BlogPosts extends Component {
-    constructor() {
-        super();
-        this.state = {
-          posts: []
-        };
-      }
+	constructor() {
+		super();
+		this.state = {
+			posts: [],
+		};
+	}
 
-    componentDidMount(){
-        fetch('/api/blogposts')
-        .then(res => res.json())
-        .then(posts => this.setState({posts}, () => console.log('Posts fetched', posts)))
-    }
+	componentDidMount() {
+		fetch('/api/blogposts')
+			.then(res => res.json())
+			.then(posts => this.setState({ posts }, () => console.log('Posts fetched', posts)));
+	}
 	render() {
-        return (
-        <React.Fragment>
-            <h1>Blog Posts</h1>     
-        </React.Fragment>
-        )}
+		return (
+			<React.Fragment>
+				<h2>Blog Posts</h2>
+				<table>
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Body</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.posts.map(post => (
+							<tr key={post.id}>
+								<td>{post.title}</td>
+								<td>{post.body}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</React.Fragment>
+		);
+	}
 }
 
 export default BlogPosts;
