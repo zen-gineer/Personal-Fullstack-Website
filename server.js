540949ce10c73063ee3af5c642d9911653544611
zen-gineer <jshtaway@gmail.com>
2019-05-09 14:07:45 -0700

    asd

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const consumer_secret = require('./consumer_secret');
 const access_token_key = require('./access_token_key');
 const access_token_secret = require('./access_token_secret');
 const spawn = require('child_process').spawn;
+const jsonData = true;
 
 app.use(express.json());
 
@@ -28,19 +29,23 @@ db.connect(err => {
 });
 
 app.post('/api/newblogpost', (req, res) => {
-	var body = req.body;
-	let sql = `INSERT INTO posts(title, body) VALUES (\"${body.title}\", \"${body.body}\")`;
-	console.log(sql);
-	db.query(sql, (err, results) => {
-		if (err) throw err;
+	if (jsonData) {
 		res.json('Posted to database');
-	});
+	} else {
+		var body = req.body;
+		let sql = `INSERT INTO posts(title, body) VALUES (\"${body.title}\", \"${body.body}\")`;
+		console.log(sql);
+		db.query(sql, (err, results) => {
+			if (err) throw err;
+			res.json('Posted to database');
+		});
+	}
 });
 
 app.post('/api/mllog', (req, res) => {
 	var body = req.body; //console.log(body);
 	resData = { sklearnLogAscii: null, sklearnLogAccuracy: null };
-	
+
 	const ls = spawn('python', ['./python/logReg.py', JSON.stringify(body)]);
 	ls.stdout.on('data', data => {
 		str = data.toString();
@@ -60,13 +65,12 @@ app.post('/api/mllog', (req, res) => {
 	// console.log(resData);
 });
 
-GetSklearnAcc = function(str){
+GetSklearnAcc = function(str) {
 	var n = str.search(/Default sklearn log accuracy:/);
-	var m = str.slice(n,).search(/:/)+1;
-	var o = str.slice(n,).search(/%/);
-	return str.slice(n+m,n+o+1);
-
-}
+	var m = str.slice(n).search(/:/) + 1;
+	var o = str.slice(n).search(/%/);
+	return str.slice(n + m, n + o + 1);
+};
 
 GetSklearnPredict = function(str) {
 	// console.log('function called', str);
@@ -77,7 +81,7 @@ GetSklearnPredict = function(str) {
 	// console.log("myStr: ", myStr)
 	// var sklearnLogRegAscii = [];
 	// for (var i = 0; i < myStr.length; i++) sklearnLogRegAscii.push(myStr.charCodeAt(i));
-	return myStr
+	return myStr;
 };
 
 app.get('/api/twitter', (req, res) => {
@@ -100,11 +104,15 @@ app.get('/api/twitter', (req, res) => {
 });
 
 app.get('/api/blogposts', (req, res) => {
-	let sql = 'SELECT * from posts';
-	db.query(sql, (err, results) => {
-		if (err) throw err;
-		res.json(results);
-	});
+	if (jsonData) {
+		res.json({});
+	} else {
+		let sql = 'SELECT * from posts';
+		db.query(sql, (err, results) => {
+			if (err) throw err;
+			res.json(results);
+		});
+	}
 });
 
 app.get('/adpost1', (req, res) => {
@@ -141,4 +149,3 @@ app.get('/createdb', (req, res) => {
 
 const port = 5000;
 app.listen(port, () => `Server running on port ${port}`);
-
