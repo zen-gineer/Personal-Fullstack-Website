14256f3758351eb40766fa452b2cb3d6677520cb
zen-gineer <jshtaway@gmail.com>
2019-05-09 15:53:33 -0700

    asd

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const access_token_key = require('./access_token_key');
 const access_token_secret = require('./access_token_secret');
 const spawn = require('child_process').spawn;
 const jsonData = true;
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
@@ -146,6 +148,24 @@ app.get('/createdb', (req, res) => {
 	});
 	res.send('Hello World');
 });
+if (isDev) {
+	app.listen(PORT, () => `Server running on port ${PORT}`);
+} else {
+	// Priority serve any static files.
+	app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-const port = 5000;
-app.listen(port, () => `Server running on port ${port}`);
+	// Answer API requests.
+	app.get('/api', function(req, res) {
+		res.set('Content-Type', 'application/json');
+		res.send('{"message":"Hello from the custom server!"}');
+	});
+
+	// All remaining requests return the React app, so it can handle routing.
+	app.get('*', function(request, response) {
+		response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+	});
+
+	app.listen(PORT, function() {
+		console.error(`Node ${isDev ? 'dev server' : 'cluster worker ' + process.pid}: listening on port ${PORT}`);
+	});
+}
